4a0d5414a4da158fd8a6457daf6f7b58d80fd323
9yutae <128476146+9yutae@users.noreply.github.com>
2024-12-24 11:13:49 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.75 ms, Memory: 3.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = n;
+    for(const auto &w:control) {
+        switch(w) {
+            case 'w':
+                answer += 1;
+                break;
+            case 's':
+                answer -= 1;
+                break;
+            case 'd':
+                answer += 10;
+                break;
+            case 'a':
+                answer -= 10;
+                break;
+        }    
+    }
+    
+    return answer;
+}
