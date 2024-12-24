2f5420058234a231547e8163694f9fd89fb261d2
9yutae <128476146+9yutae@users.noreply.github.com>
2024-12-24 11:11:59 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int n) {
+    int answer = 0;
+    // (num % n == 0) ? answer = 1 : answer = 0;
+    
+    return (num % n == 0);
+}
