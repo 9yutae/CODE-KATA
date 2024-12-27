6dd7abee5574489ebc4c48e0950564d3bb4fcb2b
9yutae <128476146+9yutae@users.noreply.github.com>
2024-12-27 09:04:06 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -4,10 +4,9 @@
 using namespace std;
 
 int solution(vector<int> absolutes, vector<bool> signs) {
-    int answer = 0;
-    for(int i=0;i<absolutes.size();i++) {
-        int tmp = signs[i] ? absolutes[i] : -absolutes[i];
-        answer += tmp;
+    int answer = 0, i=0;
+    for(const auto& w:absolutes) {
+        answer += w * (signs[i++] ? 1 : -1);
     }
     
     return answer;
