664dfff8f9da0ffae2b4dd2e587161d6323a5f01
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-02 11:14:06 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
+    
     for(const auto& w:commands) {
         vector<int> tmp;
         copy(array.begin() + w[0] - 1, array.begin() + w[1], std::back_inserter(tmp));
