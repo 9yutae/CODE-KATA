a653aff5e44ea9c3c7861a9204c69a44d89b615b
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-02 11:12:42 +0900

    Update K번째수.cpp

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -8,7 +8,7 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     for(const auto& w:commands) {
         vector<int> tmp;
-        copy(array.begin() + w[0] - 1, array.begin() + w[1], back_insert_iterator<std::vector<int>> (tmp));
+        copy(array.begin() + w[0] - 1, array.begin() + w[1], std::back_inserter(tmp));
         sort(tmp.begin(), tmp.end());
         answer.emplace_back(tmp[w[2]-1]);
     }
