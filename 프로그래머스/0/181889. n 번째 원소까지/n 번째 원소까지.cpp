862a6cf1844c1847f41499fa8b3634f48f17fc30
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-03 09:53:35 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    copy(num_list.begin(), num_list.begin() + n, back_inserter(answer));
+    return answer;
+}
