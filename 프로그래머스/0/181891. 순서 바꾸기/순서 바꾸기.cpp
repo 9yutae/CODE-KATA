1f1c4f26339046b201a498eba5adf6f294859ed9
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-03 09:44:54 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    copy(num_list.begin() + n, num_list.end(), back_inserter(answer));
+    copy(num_list.begin(), num_list.begin() + n, back_inserter(answer));
+    
+    return answer;
+}
