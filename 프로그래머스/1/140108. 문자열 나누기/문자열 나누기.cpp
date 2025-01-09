30807172d9567b4333f9c7328fa08e6c3b2cafde
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-09 10:19:08 +0900

    [level 1] Title: 문자열 나누기, Time: 0.04 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    for(int i=0;i<s.size();i++) {
+        int same = 1, diff = 0;
+        if(i == s.size()-1) {
+            answer++;
+            break;
+        }
+        
+        for(int j=i+1;j<s.size();j++) {
+            if(j == s.size()-1) {
+                answer++;
+                i = j;
+                break;
+            }
+            
+            s[j] == s[i] ? same++ : diff++;
+            if(same == diff) {
+                answer++;
+                i = j;
+                break;
+            }
+        }
+    }
+    return answer;
+}
