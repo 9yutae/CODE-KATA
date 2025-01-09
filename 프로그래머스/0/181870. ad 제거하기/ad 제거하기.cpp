d7ceb025815f1e33684d7f82b55b4bfecafeca4c
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-09 11:18:36 +0900

    [level 0] Title: ad 제거하기, Time: 1.06 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    for(auto iter=strArr.begin();iter!=strArr.end();){
+        if(iter->find("ad") != string::npos) {
+            strArr.erase(iter);
+        }
+        else iter++;
+    }
+    return strArr;
+}
