95556812c5ec87e2b2567000c3a06952b561c5d7
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-23 09:26:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    while(a != b) {
+        a = ceil((double)a/2);
+        b = ceil((double)b/2);
+        answer++;
+    }
+
+    return answer;
+}
