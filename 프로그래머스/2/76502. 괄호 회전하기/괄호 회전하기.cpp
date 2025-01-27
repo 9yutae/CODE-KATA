150e036363249930848aa1424552578f673a1877
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-27 09:25:23 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.64 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -8,41 +8,28 @@ int solution(string s) {
     int answer = 0;
     
     for(int i=0;i<s.size();i++) {
+        string temp = s.substr(i) + s.substr(0,i);
         bool isAnswer = true;
         stack<char> bracket;
-        for(int j=i;j<s.size();j++) {
-            if(s[j] == '(' || s[j] == '[' || s[j] == '{')
-                bracket.push(s[j]);
-            else if(s[j] == ')') {
+        
+        for(int j=0;j<temp.size();j++) {
+            if(!isAnswer) break;
+            
+            if(temp[j] == '(' || temp[j] == '[' || temp[j] == '{')
+                bracket.push(temp[j]);
+            else if(bracket.empty()) isAnswer = false;
+            else if(temp[j] == ')') {
                 if(bracket.top() != '(') isAnswer = false;
                 else bracket.pop();
             }
-            else if(s[j] == ']') {
+            else if(temp[j] == ']') {
                 if(bracket.top() != '[') isAnswer = false;
                 else bracket.pop();
             }
-            else if(s[j] == '}') {
+            else if(temp[j] == '}') {
                 if(bracket.top() != '{') isAnswer = false;
                 else bracket.pop();
             }
-            if(!isAnswer) break;
-        }
-        for(int j=0;j<i;j++) {
-            if(s[j] == '(' || s[j] == '[' || s[j] == '{')
-                bracket.push(s[j]);
-            else if(s[j] == ')') {
-                if(bracket.top() != '(') isAnswer = false;
-                else bracket.pop();
-            }
-            else if(s[j] == ']') {
-                if(bracket.top() != '[') isAnswer = false;
-                else bracket.pop();
-            }
-            else if(s[j] == '}') {
-                if(bracket.top() != '{') isAnswer = false;
-                else bracket.pop();
-            }
-            if(!isAnswer) break;
         }
         
         if(isAnswer && bracket.empty()) answer++;
