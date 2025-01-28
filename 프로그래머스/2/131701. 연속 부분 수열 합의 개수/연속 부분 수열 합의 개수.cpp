488e985fe91060e798474d713f2041ece853fdaa
9yutae <128476146+9yutae@users.noreply.github.com>
2025-01-28 23:16:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 154.85 ms, Memory: 23.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <set>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> elements) {
+    set<int> answer;
+    vector<int> sum = { 0 };
+    
+    for(const auto& w:elements) {
+        sum.push_back(w + sum.back());
+    }
+    
+    for(const auto& w:elements) {
+        sum.push_back(w + sum.back());
+    }
+    
+    for (int i = 0; i < elements.size(); i++)
+    {
+        for (int j = 0; j < elements.size(); j++)
+            answer.insert(sum[i + 1 + j] - sum[j]);
+    }
+    
+    return answer.size();
+}
