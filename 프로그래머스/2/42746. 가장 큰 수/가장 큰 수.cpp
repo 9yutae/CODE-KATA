e456561a9ca8c4be6c0c92204f4a0f26f610aa5a
9yutae <128476146+9yutae@users.noreply.github.com>
2025-02-19 09:38:28 +0900

    [level 2] Title: 가장 큰 수, Time: 18.11 ms, Memory: 7.46 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <iostream>
+using namespace std;
+
+bool compare(int lhs, int rhs) {
+    int _lhs = lhs, _rhs = rhs;
+    if(_lhs < 10) _lhs = lhs * 1111;
+    else if(_lhs < 100) _lhs = lhs * 101;
+    else if(_lhs < 1000) _lhs = lhs * 10 + lhs / 100;
+    
+    if(_rhs < 10) _rhs = rhs * 1111;
+    else if(_rhs < 100) _rhs = rhs * 101;
+    else if(_rhs < 1000) _rhs = rhs * 10 + rhs / 100;
+    
+    return _lhs > _rhs;
+}
+
+string solution(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end(), compare);
+    
+    string answer = "";
+    for(auto& w:numbers) {
+        answer.append(to_string(w));
+    }
+    
+    if(answer[0] == '0') return "0";
+    return answer;
+}
