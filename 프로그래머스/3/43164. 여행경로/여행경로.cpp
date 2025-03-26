07bb825f8782e97b94fb839fddc60583195a8010
Gyutae Park <128476146+9yutae@users.noreply.github.com>
2025-03-26 11:59:51 +0900

    Update 여행경로.cpp

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -4,33 +4,30 @@
 
 using namespace std;
 
-bool usedTicket[10001];
-
-bool dfs(vector<vector<string>>& tickets, vector<string>& answer, string current, int ticketCount) {
-    answer.push_back(current);
-    if (ticketCount == tickets.size()) {
-        return true;
-    }
+bool dfs(const vector<vector<string>>& tickets, vector<string>& route, vector<bool>& used, string current, int depth) {
+    route.push_back(current);
     
+    if (depth == tickets.size()) return true;
+
     for (int i = 0; i < tickets.size(); i++) {
-        if (usedTicket[i]) continue;
-        if (tickets[i][0] == current) {
-            usedTicket[i] = true;
-            if (dfs(tickets, answer, tickets[i][1], ticketCount + 1)) {
-                return true;
-            }
-            usedTicket[i] = false;
+        if (!used[i] && tickets[i][0] == current) {
+            used[i] = true;
+            if (dfs(tickets, route, used, tickets[i][1], depth + 1)) return true;
+            used[i] = false;
         }
     }
     
-    answer.pop_back();
+    route.pop_back(); // backtrack
     return false;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
-    sort(tickets.begin(), tickets.end());   
-    vector<string> answer;
-    dfs(tickets, answer, "ICN", 0);
+    sort(tickets.begin(), tickets.end()); // 사전순으로 정렬
+
+    vector<string> route;
+    vector<bool> used(tickets.size(), false);
+
+    dfs(tickets, route, used, "ICN", 0);
     
-    return answer;
+    return route;
 }
