025108c8efc21b0da8fa1f84eea4c6c2ed91a279
Gyutae Park <128476146+9yutae@users.noreply.github.com>
2025-04-14 11:12:11 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 6.25 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -1,33 +1,42 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
+
 using namespace std;
 
 vector<vector<int>> vec;
 
-void QuadTree(vector<int>& answer, int n, int y, int x) {
-    bool flag = true;
-    int prv = vec[y][x];
-    for(int i=y;i<y+n;i++) {
-        for(int j=x;j<x+n;j++) {
-            if(vec[i][j] != prv) flag = false;
-        }
+void QuadTree(vector<int>& answer, int n, int x, int y) {
+    if (n == 1) {
+        answer[vec[y][x]]++;
+        return;
     }
-    if(flag) answer[prv]++;
+    
+    bool flag = true;
+    for (int i = y; i < n + y ; i++) {
+        for (int j = x; j < n + x ; j++) {
+            if (vec[i][j] != vec[y][x]) {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag) break;
+    }
+    
+    if (flag) {
+        answer[vec[y][x]]++;
+    }
     else {
-        QuadTree(answer, n/2, y, x);
-        QuadTree(answer, n/2, y, x+n/2);
-        QuadTree(answer, n/2, y+n/2, x);
-        QuadTree(answer, n/2, y+n/2, x+n/2);
+        QuadTree(answer, n/2, x, y);
+        QuadTree(answer, n/2, x, y + n/2);
+        QuadTree(answer, n/2, x + n/2, y);
+        QuadTree(answer, n/2, x + n/2, y + n/2);
     }
 }
 
 vector<int> solution(vector<vector<int>> arr) {
-    vector<int> answer(2, 0);
+    vector<int> answer = { 0, 0 };
     vec = arr;
     QuadTree(answer, arr.size(), 0, 0);
-    
     
     return answer;
 }
