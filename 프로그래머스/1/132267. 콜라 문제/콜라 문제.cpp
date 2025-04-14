fc61a480174316deca9062497e7e2d09bb8e86e5
Gyutae Park <128476146+9yutae@users.noreply.github.com>
2025-04-14 10:41:56 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int solution(int a, int b, int n) {
     int answer = 0;
-    while(n/a != 0) {
-        answer += (n/a * b);
-        n = n/a * b + n%a;
+    while (n/a) {
+        answer += (n/a) * b;
+        n = (n/a) * b + n%a;
     }
     
     return answer;
