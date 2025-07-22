574a7c270c28e3184eed810621f997db52ad57dc
Gyutae Park <128476146+9yutae@users.noreply.github.com>
2025-07-22 21:24:56 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
@@ -1,29 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> players, int m, int k) {
-    vector<int> runningServers(24 + k, 0), newServer(24 + k, 0);
-    int idx = k;
+    queue<int> q;
+    int totalCount = 0;
     
-    for (const auto& player : players) {
-        runningServers[idx] = max(runningServers[idx - 1] - newServer[idx - k], 0);
-        int tmp = max((player - runningServers[idx] * m) / m, 0);
-
-        if (tmp > 0) {
-            runningServers[idx] += tmp;
-            newServer[idx] += tmp;
+    for (int i = 0 ; i < players.size() ; i++) {
+        while (!q.empty() && i - q.front() >= k) {
+            q.pop();
         }
         
-        idx++;
+        while (players[i] >= (q.size() + 1) * m) {
+            totalCount++;
+            q.push(i);
+        }
+        
     }
-    
-    int totalCount = 0;
-    for (const auto& w : newServer)
-        totalCount += w;
     
     return totalCount;
 }
